69b6c83ffd62c8a94a06b69050ae15ce99f1ccee
Vahag <zargaryan-vahag@mail.ru>
2021-12-17 18:57:15 +0400

    validation

--- routes/crm/operator/client.js
+++ routes/crm/operator/client.js
@@ -1,4 +1,6 @@
 const { Router } = require("express");
+const { query } = require('express-validator');
+const validationResult = require('../../../middlewares/validator');
 const ClientController = require('../../../controllers/client');
 
 const router = Router();
@@ -9,7 +11,11 @@ module.exports = (parentRouter) => {
   router.get('/balance_log', ClientController.balance_log);
   router.get('/labels', ClientController.labels);
   router.get('/limits', ClientController.limits);
-  router.get('/', ClientController.clients);
+  router.get('/',
+    query('usersId').exists().isInt(),
+    validationResult,
+    ClientController.client
+  );
 
   parentRouter.use('/client', router);
 };
